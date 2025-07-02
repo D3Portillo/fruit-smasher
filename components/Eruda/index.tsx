33d4fd6497e1fbf87f5863d6c4d03772f56ae1c1
D3Portillo <d3portillo@gmail.com>
2025-07-02 08:57:28 +0200

    chores: debug in prod with dev address

--- components/Eruda/index.tsx
+++ components/Eruda/index.tsx
@@ -1,9 +1,21 @@
+"use client"
+
 import type { PropsWithChildren } from "react"
 import { Eruda } from "./eruda-provider"
+import { useWorldUser } from "@radish-la/world-auth"
 
+const DEV_ADDRESS = "0x4c46f6d2314a41915324af999685ac447cbb79d9" as const
 export const ErudaProvider = ({ children }: PropsWithChildren) => {
-  if (process.env.NODE_ENV === "production") {
+  const user = useWorldUser()
+
+  if (
+    process.env.NODE_ENV === "production" &&
+    user?.walletAddress !== DEV_ADDRESS
+  ) {
+    // Do not render Eruda in production unless the user is the developer
+    // This is a security measure to prevent exposing developer tools in production
     return children
   }
+
   return <Eruda>{children}</Eruda>
 }
